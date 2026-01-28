9e41899a7713efdaa32006916f554c4dcd5f7926
kirill fayazov <fayazovkirill@gmail.com>
2026-01-28 16:27:24 +0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,29 +1,44 @@
-
 import { Instrument } from './types';
 
 export const INSTRUMENTS: Instrument[] = [
   {
     id: 'ng',
-    name: 'Natural Gas',
+    name: 'Природный газ',
     ticker: 'NG',
     priceStep: 0.001,
     stepPrice: 7.88527,
+    initialMarginLong: 14637.96, // Оставляем для совместимости с логикой App.tsx
+    initialMarginShort: 11768.17,
     icon: '🔥'
   },
   {
     id: 'br',
-    name: 'Brent Oil',
+    name: 'Нефть Brent',
     ticker: 'Br',
     priceStep: 0.01,
     stepPrice: 7.82267,
+    initialMarginLong: 7017.68,
+    initialMarginShort: 7017.68,
     icon: '🛢️'
   },
   {
     id: 'silver',
-    name: 'Silver',
+    name: 'Серебро',
     ticker: 'SILV',
     priceStep: 0.01,
     stepPrice: 0.78226,
+    initialMarginLong: 11766.57,
+    initialMarginShort: 11766.57,
     icon: '⚪'
+  },
+  {
+    id: 'gold',
+    name: 'Золото',
+    ticker: 'GOLD',
+    priceStep: 0.1,
+    stepPrice: 7.60382,
+    initialMarginLong: 29763.38,
+    initialMarginShort: 29763.38,
+    icon: '🥇'
   }
 ];
